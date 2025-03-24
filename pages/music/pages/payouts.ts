500dfaa1f6740668a5a0d2f9d7c59fa55c71e7a2
Gregor Bigalke <greg@bbn.family>
2025-03-24 22:00:16 +0100

    fully migrate music menu

--- pages/music/pages/payouts.ts
+++ pages/music/pages/payouts.ts
@@ -1,6 +1,7 @@
-import { asRef, Box, Content, createPage, createRoute, Entry, Grid, Label, Spinner } from "webgen/mod.ts";
-import { API, Payout, PayoutResponse, stupidErrorAlert } from "../../../spec/mod.ts";
 import { placeholder } from "shared/list.ts";
+import { Chart } from "shared/mod.ts";
+import { asRef, Box, Content, createPage, createRoute, Entry, Grid, isMobile, Label, Spinner } from "webgen/mod.ts";
+import { API, PayoutResponse, stupidErrorAlert } from "../../../spec/mod.ts";
 
 const data = asRef<"loading" | PayoutResponse[]>("loading");
 
@@ -25,16 +26,86 @@ export const payoutsPage = createPage(
         Grid(
             source.map((items) =>
                 items.length > 0
-                    ? items.map((item) =>
-                        Entry(
-                            Grid(
-                                Label(item.period).setTextSize("3xl").setFontWeight("bold"),
-                                Label(item.moneythisperiod + " Earnings"),
-                                Label(item.streams + " Streams"),
-                            ).setPadding("1rem 0"),
-                        )
+                    ? Grid(
+                        Grid(
+                            Chart({
+                                type: "bar",
+                                data: {
+                                    labels: items.map((row) => row.period.split(" to ")[0].split("Period ")[1].split("-").slice(0, 2).join("-")).reverse(),
+                                    datasets: [
+                                        {
+                                            // label: "Revenue by Month (GBP)",
+                                            data: items.map((row) => row.moneythisperiod.replace("£ ", "")).reverse(),
+                                        },
+                                    ],
+                                },
+                                options: {
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: "Revenue by Month (GBP)",
+                                            color: "white",
+                                        },
+                                        legend: {
+                                            display: false,
+                                        },
+                                    },
+                                    responsive: true,
+                                    scales: {
+                                        x: {
+                                            stacked: true,
+                                        },
+                                        y: {
+                                            stacked: true,
+                                        },
+                                    },
+                                },
+                            }),
+                            Chart({
+                                type: "bar",
+                                data: {
+                                    labels: items.map((row) => row.period.split(" to ")[0].split("Period ")[1].split("-").slice(0, 2).join("-")).reverse(),
+                                    datasets: [
+                                        {
+                                            label: "Streams by Month",
+                                            data: items.map((row) => row.streams).reverse(),
+                                        },
+                                    ],
+                                },
+                                options: {
+                                    plugins: {
+                                        title: {
+                                            display: true,
+                                            text: "Streams by Month",
+                                            color: "white",
+                                        },
+                                        legend: {
+                                            display: false,
+                                        },
+                                    },
+                                    responsive: true,
+                                    scales: {
+                                        x: {
+                                            stacked: true,
+                                        },
+                                        y: {
+                                            stacked: true,
+                                        },
+                                    },
+                                },
+                            }),
+                        ).setEvenColumns(isMobile.map((x) => x ? 1 : 2)),
+                        Box(items.map((item) =>
+                            Entry(
+                                Grid(
+                                    Label(item.period).setTextSize("3xl").setFontWeight("bold"),
+                                    Label(`${item.moneythisperiod} Earnings`),
+                                    Label(`${item.streams} Streams`),
+                                ).setPadding("1rem 0"),
+                            )
+                        )),
                     )
-                    : placeholder("No Payouts", `You don’t have any Payouts yet.`)
+                    : placeholder("No Payouts", `You don't have any Payouts yet.`)
             ),
         ),
     ),
